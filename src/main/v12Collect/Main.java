796dbc350d4f49eb89f7de8880f9012bf2046b87
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-12-27 13:57:09 -0500

    Finish course

--- src/main/v12Collect/Main.java
+++ src/main/v12Collect/Main.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,14 +20,14 @@ public class Main {
         List<Book> bookList = Arrays.asList(
                 new Book("345-89", "Sufriendo a Pedro", 2018, Genre.TERROR),
                 new Book("923-45", "Los papeles por delante", 1998, Genre.THRILLER),
-                new Book("978-25", "la vida de Baldomero", 2017, Genre.COMEDY),
+                new Book("978-25", "La vida de Baldomero", 2017, Genre.COMEDY),
                 new Book("923-45", "Los papeles por delante", 1998, Genre.THRILLER),
-                new Book("978-25", "la vida de Baldomero", 2017, Genre.COMEDY)
+                new Book("978-25", "La vida de Baldomero", 2017, Genre.COMEDY)
         );
 
         List<Book> result = bookList.stream()
                 .filter(book -> book.getYearPublisher() < 2000)
-                .collect(Collectors.toList());
+                .collect(toList());
 
         System.out.println("result = " + result);
 
@@ -239,6 +241,190 @@ public class Main {
 
         System.out.println("resultFiltering = " + resultFiltering);
 
+        //Ordenación con comparadores avanzados
+        //retorna -1 si book1 es mayor, retorna 0 si son iguales, retorna 1 si book 2 es mayor
+
+        //Ordenar por fecha de publicación
+
+        System.out.println("------------------------------------------------------------");
+
+        bookList.stream()
+                .sorted((book1, book2) -> Integer.compare(book1.getYearPublisher(), book2.getYearPublisher()))
+                .forEach(System.out::println);
+
+        //Ordenar por titulo
+
+        System.out.println("------------------------------------------------------------");
+
+        bookList.stream()
+                .sorted((book1, book2) -> book1.getTitle().compareTo(book2.getTitle()))
+                .forEach(System.out::println);
+
+        //Estas dos funcionan siempre y cuando la clase book implemente comparable entre sus métodos
+
+        System.out.println("------------------------------------------------------------");
+
+        bookList.stream()
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+
+        System.out.println("------------------------------------------------------------");
+
+        bookList.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
+
+        //Comparar por titulo y por fecha de publicación, si los titulos son iguales ordena por fecha de publicación
+
+        System.out.println("------------------------------------------------------------");
+
+        bookList.stream()
+                .sorted((book1, book2) -> {
+                    int titleComparison = book1.compareTo(book2);
+                    if (titleComparison != 0) {
+                        return titleComparison;
+                    } else {
+                        return book2.getYearPublisher() - book1.getYearPublisher();//orden inverso de año de publicación
+                        //return book1.getYearPublisher() - book2.getYearPublisher();//orden normal de año de publicación
+                    }
+                })
+                .forEach(System.out::println);
+
+        //Pero la interfaz comparator ya tiene métodos que nos ayudan a poner varios criterios de comparación
+        //para evitar poner else if
+
+        System.out.println("--------------------------------------------------");
+
+        bookList.stream()
+                .sorted(Comparator.comparing(Book::getTitle)
+                        .thenComparing(Book::getYearPublisher, Comparator.reverseOrder()))
+                .forEach(System.out::println);
+
+        //Métodos map y filter optional
+
+        System.out.println("----------------------------------------------------");
+
+        bookList.stream()
+                .sorted(Comparator.comparing(Book::getTitle)
+                        .thenComparing(Book::getYearPublisher, Comparator.reverseOrder()))
+                .findFirst()
+                .filter(book -> book.getGenre() == Genre.COMEDY)
+                .map(book -> book.getTitle())
+                .ifPresentOrElse(
+                        title -> System.out.println("book = " + title),
+                        () -> System.out.println("The list is void")
+                );
+
+        //Métodos Stream de un optional
+        //Lo que se hace acá es filtrar únicamente los que esté presentes dentro de la lista para luego ponerlos en otra
+
+        List<String> isbnList = List.of("345-89", "978-25", "noexiste");
+
+        List<Book> resultStreamOptional = isbnList.stream()
+                .map(isbn -> findBookByIsbn(isbn))
+                .filter(optional -> optional.isPresent())
+                .map(optional -> optional.get())
+                .collect(toList());
+
+        System.out.println("resultStreamOptional = " + resultStreamOptional);
+
+        //Ahora con un flatMap
+        //Lo que entendí del flatMap, convierte la lista de Streams y los convierte en un único Stream de los elementos
+
+        List<Book> resultStreamOptionalFlatMap = isbnList.stream()
+                .map(isbn -> findBookByIsbn(isbn))
+                .flatMap(optional -> optional.stream())
+                .collect(toList());
+
+        System.out.println("resultStreamOptionalFlatMap = " + resultStreamOptionalFlatMap);
+
+        //recolectar Stram y aplicar función
+        //mapping permite ejecutar sobre cada elemento un determinada function antes de recolectarlos
+        //CollectingAndThen hace lo contrario a lo que hace el mapping, primero recolecta y luego se aplica la función
+
+        String resultCollectingAndThen = bookList.stream()
+                .collect(Collectors.collectingAndThen(
+                        Collectors.counting(),
+                        value -> value + " books"
+                ));
+
+        System.out.println("resultCollectingAndThen = " + resultCollectingAndThen);
+
+        Map<Integer, String> resultCollectingAndThen2 = bookList.stream()
+                .collect(Collectors.groupingBy(
+                        book -> book.getYearPublisher(),
+                        Collectors.collectingAndThen(
+                                Collectors.counting(),
+                                value -> value + " books"
+                        )
+                ));
+
+        System.out.println("resultCollectingAndThen2 = " + resultCollectingAndThen2);
+
+        //Recolector para versión 12 o superior
+        //Queremos calcular la diferencia de años entre el libro más antiguo y el libro más moderno
+        //Esto lo logramos con teeing
+        //Con el map en el maxOptional, se logra validar que el maxOptional tenga valor, es decir, si verifica que la
+        //Lista no esté vacía
+
+        bookList.stream()
+                .map(book -> book.getYearPublisher())
+                .collect(Collectors.teeing(
+                                Collectors.maxBy(Integer::compare),
+                                Collectors.minBy(Integer::compare),
+                                (maxOptional, minOptional) -> maxOptional.map(value -> value - minOptional.get())
+                        )
+                )
+                .ifPresentOrElse(
+                        difference -> System.out.println("difference = " + difference),
+                        () -> System.out.println("The list is void")    //Esto es un runable
+                );
+
+        //Filtrar tras recolectar a mapa
+        //Objetico: Obtener todos los géneros de libros ordenados de mayor a menos número de libros,
+        //pero quedarnos con sólo los géneros que terngan más de un libro
+
+        List<Book> bookListFiltrarOrdenar_A_Mapa = Arrays.asList(
+                new Book("345-89", "Sufriendo a Pedro", 2018, Genre.TERROR),
+                new Book("923-45", "Los papeles por delante", 1998, Genre.THRILLER),
+                new Book("978-25", "La vida de Baldomero", 2017, Genre.COMEDY),
+                new Book("923-45", "Los papeles por delante", 1998, Genre.THRILLER),
+                new Book("923-46", "Segundo libro de Thriller", 1998, Genre.THRILLER),
+                new Book("978-25", "La vida de Baldomero", 2017, Genre.COMEDY)
+        );
+
+        Map<Genre, Long> resultFiltrarTrasRecolectar = bookListFiltrarOrdenar_A_Mapa.stream()
+                .collect(Collectors.groupingBy(
+                                book -> book.getGenre(),
+                                Collectors.counting()
+                        )
+                )
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() > 1)
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (entry1, entry2) -> entry1,//Esto es para poder ordenar el mapa, no tiene nigún efecto sobre el mapa, se hace para poder pasar el cuarto argumento
+                                LinkedHashMap::new //Este es para tenerlo ordenado
+                        )
+                );
+
+        System.out.println("resultFiltrarTrasRecolectar = " + resultFiltrarTrasRecolectar);
     }
 
+    Optional<Book> findBookByIsbn(String isbn) {
+        List<Book> bookList = Arrays.asList(
+                new Book("345-89", "Sufriendo a Pedro", 2018, Genre.TERROR),
+                new Book("923-45", "Los papeles por delante", 1998, Genre.THRILLER),
+                new Book("978-25", "La vida de Baldomero", 2017, Genre.COMEDY),
+                new Book("923-45", "Los papeles por delante", 1998, Genre.THRILLER),
+                new Book("978-25", "La vida de Baldomero", 2017, Genre.COMEDY)
+        );
+
+        return bookList.stream()
+                .filter(book -> book.getIsbn().equals(isbn))
+                .findFirst();
+    }
 }
